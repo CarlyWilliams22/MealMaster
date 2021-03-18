3e0ce182d1aea7aad7ed2b1be4d9f6bf26d7601c
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-03-18 00:15:34 -0400

    Pausemanager -> PauseManagerScript

--- Assets/Scripts/PauseManagerScript.cs
+++ Assets/Scripts/PauseManagerScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseManager : MonoBehaviour
+public class PauseManagerScript : MonoBehaviour
 {
     PlayerLookScript playerLookScript;
     private bool isPaused;
