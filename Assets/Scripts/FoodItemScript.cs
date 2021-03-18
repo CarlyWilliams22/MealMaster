cd404f5a295872208c43b9a9dbb3f1f840c8e2ae
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-03-18 01:04:29 -0400

    initial FoodItemScript

---
+++ Assets/Scripts/FoodItemScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodItemScript : MonoBehaviour
+{
+    public enum FoodItemType {
+        DRINK, BURGER
+    }
+
+    public float costWholesale; // restocking price
+    public float costRetail;    // price customer pays
+    public FoodItemType type;
+    public float cookDuration;
+}
