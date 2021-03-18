e2f956b87e47f4e889cb78683fcab3a6bd514490
dreigle <70277016+dreigle@users.noreply.github.com>
2021-03-18 11:28:39 -0400

    Bank Account

---
+++ Assets/Scripts/BankAccountScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BankAccountScript : MonoBehaviour
+{
+    public float currentBalance;
+    public float balanceAtLevelStart;
+
+
+    public float GetProfit()
+    {
+        return currentBalance - balanceAtLevelStart;
+    }
+
+    public void SetBalanceAtLevelStart(float startingBalance)
+    {
+        balanceAtLevelStart = startingBalance;
+    }
+
+    public void CreditAccount(float credit)
+    {
+        currentBalance += credit;
+    }
+
+    public void WithdrawFromAccount(float withdrawal)
+    {
+        currentBalance -= withdrawal;
+    }
+
+}
