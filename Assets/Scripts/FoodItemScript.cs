0513a748409b83b42d1f34ab1ab517a8bda175d4
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-03-19 09:09:57 -0400

    make FoodItem attributes static members

--- Assets/Scripts/FoodItemScript.cs
+++ Assets/Scripts/FoodItemScript.cs
@@ -8,8 +8,40 @@ public class FoodItemScript : MonoBehaviour
         DRINK, BURGER
     }
 
-    public float costWholesale; // restocking price
-    public float costRetail;    // price customer pays
+    // restocking prices
+    public static readonly Dictionary<FoodItemType, float> wholesalePrices = new Dictionary<FoodItemType, float>
+    {
+        { FoodItemType.DRINK, 0.25f },
+        { FoodItemType.BURGER, 0.75f }
+    };
+
+    // what the customer pays
+    public static readonly Dictionary<FoodItemType, float> retailPrices = new Dictionary<FoodItemType, float>
+    {
+        { FoodItemType.DRINK, 1.00f },
+        { FoodItemType.BURGER, 2.50f }
+    };
+
+    // cook durations in seconds
+    public static readonly Dictionary<FoodItemType, float> cookDurations = new Dictionary<FoodItemType, float>
+    {
+        { FoodItemType.DRINK, 3.0f },
+        { FoodItemType.BURGER, 10.0f }
+    };
+
     public FoodItemType type;
-    public float cookDuration;
+    public float cookDuration
+    {
+        get => cookDurations[type];
+    }
+
+    public float retailPrice
+    {
+        get => retailPrices[type];
+    }
+
+    public float wholesalePrice
+    {
+        get => wholesalePrices[type];
+    }
 }
