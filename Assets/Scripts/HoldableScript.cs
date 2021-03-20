8c39c2bdfb709fd72fbc35e6dc2afecdf954f5d1
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-03-20 17:36:51 -0400

    allow gravity on holdables

--- Assets/Scripts/HoldableScript.cs
+++ Assets/Scripts/HoldableScript.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class HoldableScript : MonoBehaviour
 {
     private Transform parent;
+    private Rigidbody rbody;
+
+    private bool origIsKinematic;
+    private bool origUseGravity;
+
+    private void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
 
     public void Grab(GameObject holder, Vector3 offset)
     {
         parent = transform.parent;
         transform.position = holder.transform.position + (transform.position - offset);
         transform.SetParent(holder.transform);
-        // TODO enable kinematic and disable gravity for rigidbodies etc
+        if (rbody)
+        {
+            origIsKinematic = rbody.isKinematic;
+            rbody.isKinematic = true;
+            origUseGravity = rbody.useGravity;
+            rbody.useGravity = false;
+        }
     }
 
     public bool Release()
     {
         transform.SetParent(parent);
-        // TODO disable kinematic and re-enable gravity for rigidbodies etc
+        if (rbody)
+        {
+            rbody.isKinematic = origIsKinematic;
+            rbody.useGravity = origUseGravity;
+        }
         // TODO can the object be released here? If not return false
         return true;
     }
