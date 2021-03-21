41da6e54b11332aee137091ab81d0094c6c003e0
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-03-20 20:07:42 -0400

    add player gravity

--- Assets/Scripts/PlayerMoveScript.cs
+++ Assets/Scripts/PlayerMoveScript.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerMoveScript : MonoBehaviour
 {
     public float speed;
+    public float gravity;
 
     private CharacterController controller;
+    private float ySpeed = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +19,17 @@ public class PlayerMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (controller.isGrounded)
+        {
+            ySpeed = -1;
+        } else
+        {
+            ySpeed += Time.deltaTime * gravity;
+        }
+
         Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         Vector3 moveVec = transform.rotation * Vector3.ClampMagnitude(moveInput * speed, speed);
+        moveVec.y += ySpeed;
         controller.Move(moveVec * Time.deltaTime);
     }
 }
