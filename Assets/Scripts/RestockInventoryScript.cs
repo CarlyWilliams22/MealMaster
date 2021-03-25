e76f0e7cf3c0c6b4e6137b58d48d1cecd1184705
dreigle <70277016+dreigle@users.noreply.github.com>
2021-03-25 10:55:54 -0400

    Calculate restocking price

---
+++ Assets/Scripts/RestockInventoryScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RestockInventoryScript : MonoBehaviour
+{
+    public Text moneyCalcLabel;
+    public Text orderAmountLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onChangeOrderAmount()
+    {
+        float price = FoodItemScript.wholesalePrices[FoodItemScript.FoodItemType.BURGER];
+        moneyCalcLabel.text = "x " + price.ToString("C") + " = " + (int.Parse(orderAmountLabel.text) * price).ToString("C");
+    }
+}
