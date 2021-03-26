e04405b8b453d3670012555ea77ef784004a1f16
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2021-03-25 23:29:22 -0400

    All character animations now work

--- Assets/Scripts/CustomerSpawnManager.cs
+++ Assets/Scripts/CustomerSpawnManager.cs
@@ -13,7 +13,7 @@ public class CustomerSpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject[] prefabs = { customer1 };//customer2, customer3};
+        GameObject[] prefabs = { customer1, customer2, customer3};
         customerPool = new ObjectPool(prefabs, 1, false);
     }
 
