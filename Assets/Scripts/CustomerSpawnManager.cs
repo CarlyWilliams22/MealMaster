c250c2813d6e759ef916a2d121f274afae99530a
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2021-04-07 13:31:41 -0400

    Customers go to different spots in the camera

--- Assets/Scripts/CustomerSpawnManager.cs
+++ Assets/Scripts/CustomerSpawnManager.cs
@@ -6,25 +6,48 @@ using UnityEngine.AI;
 public class CustomerSpawnManager : MonoBehaviour
 {
 
-    public GameObject customer1, customer2, customer3, point1;
+    public GameObject customer1, customer2, customer3, point1, point2, point3;
     ObjectPool customerPool;
     GameObject newCustomer;
+    GameObject[] spots = new GameObject[3];
+    bool readyToSpawn = true;
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject[] prefabs = { customer1, customer2, customer3};
-        customerPool = new ObjectPool(prefabs, 1, false);
+        customerPool = new ObjectPool(prefabs, 3, false);
+        spots[0] = point1;
+        spots[1] = point2;
+        spots[2] = point3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        newCustomer = customerPool.GetObject();
-        if (newCustomer)
+        if (readyToSpawn)
         {
-            newCustomer.GetComponent<NavMeshAgent>().Warp(transform.position);
-            newCustomer.GetComponent<NavMeshAgent>().SetDestination(point1.transform.position);
+            newCustomer = customerPool.GetObject();
+            if (newCustomer)
+            {
+                for (int i = 0; i < spots.Length; i++)
+                {
+                    if (spots[i].GetComponent<SpotScript>().getOpen())
+                    {
+                        newCustomer.GetComponent<NavMeshAgent>().Warp(transform.position);
+                        newCustomer.GetComponent<NavMeshAgent>().SetDestination(spots[i].transform.position);
+                        spots[i].GetComponent<SpotScript>().taken();
+                        break;
+                    }
+                }
+            }
+            readyToSpawn = false;
+            Invoke("nextCustomer", 3);
         }
+    }
+
+    void nextCustomer()
+    {
+        readyToSpawn = true;
     }
 }
---
+++ Assets/Scripts/SpotScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotScript : MonoBehaviour
+{
+
+    private bool open = true;
+
+    public void taken()
+    {
+        open = false;
+    }
+
+    public void free()
+    {
+        open = true;
+    }
+
+    public bool getOpen() { return open; }
+
+}
